a08d3d5706f7481d990f37e70531dd978ee995be
Jonas Triki <kosi_722@hotmail.com>
2018-03-10 17:02:01 +0100

    Removed old files from docs and added XD prototype

--- chess_game/core/src/scenes/GameScene.java
+++ chess_game/core/src/scenes/GameScene.java
@@ -6,67 +6,67 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import chessGame.Chess;
 
 
-public class GameScene implements Screen{
-	
+public class GameScene implements Screen {
+
 	private Chess game;
 	//private Sprite bPawn, wPawn, bRook, wRook, wBishop, bBishop, bKnight, wKnight, bQueen, wQueen;
-	
+
 	public GameScene (Chess mainGame){
-		
-	game = mainGame;
-	/*
-	 * bPawn = new Sprite(new Texture("bPawn.jpg");
-	 * wPawn = new Sprite(new Texture("wPawn.jpg");
-	 * bRook = new Sprite(new Texture("bRook.jpg");
-	 * wRook = new Sprite(new Texture("wRook.jpg");
-	 * wBishop = new Sprite(new Texture("wBishop.jpg");
-	 * bBishop = new Sprite(new Texture("bBishop.jpg");
-	 * bKnight = new Sprite(new Texture("bKnight.jpg");
-	 * wKnight = new Sprite(new Texture("wKnight.jpg");
-	 * bQueen = new Sprite (new Texture("bQueen.jpg");
-	 * wQueen = new Sprite (new Texture("wQueen.jpg");
-	 */
+
+		game = mainGame;
+		/*
+		 * bPawn = new Sprite(new Texture("bPawn.jpg");
+		 * wPawn = new Sprite(new Texture("wPawn.jpg");
+		 * bRook = new Sprite(new Texture("bRook.jpg");
+		 * wRook = new Sprite(new Texture("wRook.jpg");
+		 * wBishop = new Sprite(new Texture("wBishop.jpg");
+		 * bBishop = new Sprite(new Texture("bBishop.jpg");
+		 * bKnight = new Sprite(new Texture("bKnight.jpg");
+		 * wKnight = new Sprite(new Texture("wKnight.jpg");
+		 * bQueen = new Sprite (new Texture("bQueen.jpg");
+		 * wQueen = new Sprite (new Texture("wQueen.jpg");
+		 */
 	}
 
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void render(float delta) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
--- chess_game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ chess_game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,17 +1,17 @@
 package com.mygdx.game.desktop;
 
+import chessGame.GameInformation;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import assistance.GameInformation;
 import chessGame.Chess;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Chess(), config);
-		config.width = chessGame.GameInformation.WIDTH;
-		config.height = chessGame.GameInformation.HEIGHT;
+		config.width = GameInformation.WIDTH;
+		config.height = GameInformation.HEIGHT;
 		config.resizable = false; 
 	}
 }
