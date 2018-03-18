676a92adf32e49a551f4e321d20abdfa6be1ff53
Eirin Sognnes <eirin.Sognnes@student.uib.no>
2018-03-18 23:51:07 +0100

    queen updates

--- chess_game/core/src/game/ChessGame.java
+++ chess_game/core/src/game/ChessGame.java
@@ -3,6 +3,7 @@ package game;
 import boardstructure.IBoard;
 import boardstructure.Move;
 import pieces.PieceColor;
+import player.AILevel;
 import setups.DefaultSetup;
 
 public class ChessGame implements CheckerboardListener {
@@ -11,17 +12,21 @@ public class ChessGame implements CheckerboardListener {
 	String player2;
 	PieceColor playerOneColor;
 	GameType gameType;
-	
-	public ChessGame(String player1, String player2, GameType gameType, PieceColor playerOneColor) {
+	AILevel level;
+
+	public ChessGame(String player1, String player2, GameType gameType, PieceColor playerOneColor, AILevel level) {
 		this.player1 = player1;
-		if(player2 == null)
+		if(player2 == null) {
 			this.player2 = "computer";
+			this.level = level;
+		}
 		else 
 			this.player2 = player2;
 		this.gameType = gameType;
-		
+
+
 		this.playerOneColor = playerOneColor;
-		
+
 		//board for standard chess
 		this.board = (new DefaultSetup()).getInitialPosition(playerOneColor);
 	}
@@ -35,4 +40,6 @@ public class ChessGame implements CheckerboardListener {
 	public void onMoveRequested(int fromX, int fromY, int toX, int toY) {
 
 	}
+
+
 }
--- chess_game/core/src/pieces/pieceClasses/Queen.java
+++ chess_game/core/src/pieces/pieceClasses/Queen.java
@@ -6,10 +6,11 @@ import boardstructure.IBoard;
 import boardstructure.Move;
 import boardstructure.Square;
 import pieces.AbstractPiece;
+import pieces.IPiece;
 import pieces.PieceColor;
 
 public class Queen extends AbstractPiece {
-	
+
 	public Queen(PieceColor color) {
 		inPlay = true;
 		this.color = color;
@@ -19,19 +20,35 @@ public class Queen extends AbstractPiece {
 	protected ArrayList<Move> allFreeMoves(int x, int y, IBoard board, PieceColor playerOne) {
 		ArrayList<Move> reachable = new ArrayList<Move>();
 		Square origin = board.getSquare(x, y);
-		reachable.addAll(reachableSquares(x, y, origin, board, true, false));
-		reachable.addAll(reachableSquares(y, x, origin, board, false, true));
-		reachable.addAll(reachableSquares(x, y, origin, board, false, false));
+		reachable.addAll(reachableSquaresStraight(x, y, origin, board, true));
+		reachable.addAll(reachableSquaresStraight(y, x, origin, board, false));
+		reachable.addAll(reachableSquaresDiagonalBottomLeftToTopRight(x, y, origin, board));
+		reachable.addAll(reachableSquaresDiagonalBottomRightToTopLeft(x, y, origin, board));
 		return reachable;
-		
+
 	}
+	/**
+	 * Reachable squares in a straight line from Queen.
+	 * @param startPoint
+	 * @param axis
+	 * @param origin
+	 * @param board
+	 * @param horizontal
+	 * @return
+	 */
+	public ArrayList<Move> reachableSquaresStraight(int startPoint, int axis, Square origin, IBoard board, boolean horizontal) {
+		ArrayList<Move> ok = new ArrayList<>();
+		Square dest;
 
-	public ArrayList<Move> reachableSquares(int startPoint, int axis, Square origin, IBoard board, boolean horizontal,
-			boolean diagonal) {
-		ArrayList<Move> ok = new ArrayList<Move>();
-
+		// check axis upwards
 		for (int i = startPoint + 1; i < board.getDimension(); i++) {
-			Square dest = getSquare(axis, board, horizontal, diagonal, i);
+
+			// looking on y-axis or x-axis
+			if (horizontal) {
+				dest = board.getSquare(i, axis);
+			} else {
+				dest = board.getSquare(axis, i);
+			}
 
 			if (!dest.isEmpty()) {
 				if (getColor() != dest.getPiece().getColor())
@@ -41,9 +58,16 @@ public class Queen extends AbstractPiece {
 				ok.add(getMove(origin, dest, board));
 			}
 		}
-		
+
+		// check axis downwards
 		for (int i = startPoint - 1; i >= 0; i--) {
-			Square dest = getSquare(axis, board, horizontal, diagonal, i);
+
+			// check if we're looking on y-axis or x-axis
+			if (horizontal) {
+				dest = board.getSquare(i, axis);
+			} else {
+				dest = board.getSquare(axis, i);
+			}
 
 			if (!dest.isEmpty()) {
 				if (getColor() != dest.getPiece().getColor())
@@ -51,21 +75,104 @@ public class Queen extends AbstractPiece {
 				break;
 			} else {
 				ok.add(getMove(origin, dest, board));
+			}
+		}		
+		return ok;
+	}
+
+	/**
+	 * Gets all the moves from 
+	 * @param startPoint
+	 * @param axis
+	 * @param origin
+	 * @param board
+	 * @return
+	 */
+	public ArrayList<Move>reachableSquaresDiagonalBottomLeftToTopRight(int x, int y, Square origin, IBoard board) {
+		ArrayList<Move> ok = new ArrayList<>();
+		int i = x+1; int j = y+1;
+		Square dest = null;
+
+		if(board.withinBoard(i,j)) {
+			while (i < board.getWidth() && j < board.getHeight()) {
+				dest = board.getSquare(i, j);
+				if(!dest.isEmpty()) {
+					if (dest.getPiece().getColor() != getColor()) {
+						ok.add(getMove(origin, dest, board));
+					}
+					break;
+				} else if(dest.isEmpty()) {
+					ok.add(getMove(origin, dest, board));
+				}
+				i++; j++;
+			}
+		}
+
+
+		i = x-1; j = y+1;
+		if(board.withinBoard(i,j)) {
+			while (i >= 0 && j >= 0) {
+				dest = board.getSquare(i, j);
+				if(!dest.isEmpty()) {
+					if (dest.getPiece().getColor() != getColor()) {
+						ok.add(getMove(origin, dest, board));
+					}
+					break; 
+				} else if(dest.isEmpty()) {
+					ok.add(getMove(origin, dest, board));
+				}
+				i--; j--;
 			}
 		}
 		return ok;
 	}
 
-	private Square getSquare(int axis, IBoard board, boolean horizontal, boolean diagonal, int i) {
-		Square newSq;
-		if (horizontal) {
-            newSq = board.getSquare(i, axis);
-        } else if (diagonal) {
-            newSq = board.getSquare(i, i);
-        } else {
-            newSq = board.getSquare(axis, i);
-        }
-		return newSq;
+	/**
+	 * Get
+	 * @param startPoint
+	 * @param axis
+	 * @param origin
+	 * @param board
+	 * @return the possible moves on this diagonal
+	 */
+	public ArrayList<Move>reachableSquaresDiagonalBottomRightToTopLeft(int x, int y, Square origin, IBoard board) {
+		ArrayList<Move> ok = new ArrayList<>();
+		int i = x+1; int j = y-1;
+		Square dest = null;
+
+		if(board.withinBoard(i, j)) {
+			while (i < board.getWidth() && j >= 0) {
+				dest = board.getSquare(i, j);
+				if(!dest.isEmpty()) {
+					if (dest.getPiece().getColor() != getColor()) {
+						ok.add(getMove(origin, dest, board));
+					}
+					break;
+				}
+				if(dest.isEmpty()) {
+					ok.add(getMove(origin, dest, board));
+				}
+				i++; j--;
+			}
+		}
+
+		i = x-1; j = y+1;
+		if(board.withinBoard(i,j)) {
+			while (i >= 0 && j < board.getHeight()) {
+				dest = board.getSquare(i, j);
+				if(!dest.isEmpty()) {
+					if (dest.getPiece().getColor() != getColor()) {
+						ok.add(getMove(origin, dest, board));
+					}
+					break;
+				}
+				if(dest.isEmpty()) {
+					ok.add(getMove(origin, dest, board));
+				}
+				i--; j++;
+			}
+		}
+		return ok;
 	}
 
 	@Override
