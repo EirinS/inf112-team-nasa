934bf078741c68a2741d9e93e8280cb62dadf5ac
Eirin Sognnes <eirin.Sognnes@student.uib.no>
2018-03-18 22:10:52 +0100

    added ChessGame-class and GameType enum

---
+++ chess_game/core/src/game/ChessGame.java
@@ -0,0 +1,41 @@
+package game;
+
+import boardstructure.IBoard;
+import boardstructure.Move;
+import pieces.PieceColor;
+import setups.DefaultSetup;
+
+public class ChessGame implements CheckerboardListener {
+	IBoard board;
+	String player1;
+	String player2;
+	PieceColor playerOneColor;
+	GameType gameType;
+	
+	public ChessGame(String player1, String player2, GameType gameType, PieceColor playerOneColor) {
+		this.player1 = player1;
+		if(player2 == null)
+			this.player2 = "computer";
+		else 
+			this.player2 = player2;
+		this.gameType = gameType;
+		
+		this.playerOneColor = playerOneColor;
+		
+		//board for standard chess
+		this.board = (new DefaultSetup()).getInitialPosition(playerOneColor);
+	}
+
+
+	@Override
+	public void onPieceClick(int x, int y) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onMoveRequested(Move m) {
+		// TODO Auto-generated method stub
+	}
+	
+
+}
