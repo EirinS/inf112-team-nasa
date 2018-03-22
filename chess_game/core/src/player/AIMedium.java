52515ad40ee33019f039b899261615892726f415
Jonas Trædal <jtr009@student.uib.no>
2018-03-22 17:04:07 +0100

    Fixed bug in AIMedium AIMedium no longer suicides to pawns! Do not think more changes are needed for AIMedium

--- chess_game/core/src/player/AIMedium.java
+++ chess_game/core/src/player/AIMedium.java
@@ -158,7 +158,7 @@ public class AIMedium implements AI, Playable {
 	public ArrayList<Board> getPossibleBoards(IBoard currentBoard,List<Move> possibleMoves, PieceColor playerTurn){// this is really messy, new possible boards are created based on all possible outcomes(moves), i did not find a good way to do this, i make a copy shallow copy of unaltered squares and a create new squares where there is changes. i think this will work. the pieces are also only shallow copies. might need to be redone
 		ArrayList<Board> possibleBoards = new ArrayList<Board>();
  		for(Move move : possibleMoves) {
-			Board possibleBoard = new Board(currentBoard.getDimension(),playerTurn);
+			Board possibleBoard = new Board(currentBoard.getDimension(),currentBoard.getPlayerOne());
 			for(Square square : currentBoard.getSquares()) {
 				if (move.getFrom()==square) {
 				}else if (move.getTo()==square) {																																								
