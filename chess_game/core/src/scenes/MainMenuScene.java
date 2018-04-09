5f0a1f9e7ee811490ff596e4201ad583995b6524
Jonas Folkvord Triki <jonas.triki@student.uib.no>
2018-04-09 14:11:35 +0200

    Added Database module and PostgreSQL lib files

--- chess_game/core/src/scenes/MainMenuScene.java
+++ chess_game/core/src/scenes/MainMenuScene.java
@@ -17,6 +17,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import db.Database;
 import game.Chess;
 
 import game.chessGame.GameInfo;
